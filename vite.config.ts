4f15e21759f04f2e430da12425f3436e48d6802a
Vincent-the-gamer <erxianqiaottjt@163.com>
2025-10-26 23:38:28 +0800

    chore: release v0.1.0

--- vite.config.ts
+++ vite.config.ts
@@ -10,9 +10,11 @@ export default defineConfig({
     monkey({
       entry: 'src/main.ts',
       userscript: {
-        icon: 'https://vitejs.dev/logo.svg',
-        namespace: 'npm/vite-plugin-monkey',
-        match: ['https://live.bilibili.com/*'],
+        icon: 'https://img.moegirl.org.cn/common/6/61/%E4%B8%89%E5%8F%B8%E7%BB%AB%E6%BF%91_%E8%A7%92%E8%89%B2%E6%AD%8C%E4%B8%93%E8%BE%91%E5%B0%81%E9%9D%A2.jpg',
+        namespace: 'https://github.com/Vincent-the-gamer/ayase',
+        match: [
+          'https://live.bilibili.com/*'
+        ],
       },
     }),
   ],
